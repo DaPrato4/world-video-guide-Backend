cb03f0eef24efa83b3693c0661245751faee990b
DaPrato4 <daprato363@gmail.com>
2026-05-10 13:54:45 +0200

    Fix server port configuration

--- index.js
+++ index.js
@@ -171,6 +171,7 @@ app.get('/', (req, res) => {
   res.send('Server in ascolto. La sentinella è attiva sui video!');
 });
 
-app.listen(3000, () => {
-  console.log('Server acceso sulla porta 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server acceso sulla porta ${PORT}`);
 });
